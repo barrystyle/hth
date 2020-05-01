81a15ff89af52f43bac20be6ea076965a763e515
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-30 21:34:02 -0400

    Update walletframe.cpp

--- src/qt/walletframe.cpp
+++ src/qt/walletframe.cpp
@@ -108,6 +108,13 @@ void WalletFrame::showOutOfSyncWarning(bool fShow)
         i.value()->showOutOfSyncWarning(fShow);
 }
 
+void WalletFrame::gotoGovernancePage()
+{
+    QMap<QString, WalletView*>::const_iterator i;
+    for (i = mapWalletViews.constBegin(); i != mapWalletViews.constEnd(); ++i)
+        i.value()->gotoGovernancePage();
+}
+
 void WalletFrame::gotoOverviewAPage()
 {
     QMap<QString, WalletView*>::const_iterator i;
